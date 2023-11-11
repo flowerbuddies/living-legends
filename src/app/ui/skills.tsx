7065ea52e3268ea49cc8b717e18fe338fae18247
Teemu Spoof <steemum@hotmail.com>
2023-11-11 19:36:07 +0200

    added buttons to level up skills

--- src/app/ui/skills.tsx
+++ src/app/ui/skills.tsx
@@ -6,6 +6,8 @@ import { PlayerInfo } from "../../lib/player";
 export default function Skills(props: { id: number }) {
 	const [player, setPlayer] = useState<PlayerInfo>();
 
+	let lolwhat = () => {};
+
 	useEffect(() => {
 		fetch(`/api/player?id=${props.id}`)
 			.then((res) => res.json())
@@ -13,19 +15,30 @@ export default function Skills(props: { id: number }) {
 				setPlayer(player);
 			});
 	}, []);
+
 	return (
 		<>
 			<div className="flex flex-col">
 				<div className="self-center">
 					<h3>Skills</h3>
 				</div>
-				<div className="shadow self-center text-center">
-					<h4>💪</h4>
-					<p>{player?.attackModifier}</p>
+				<div className="shadow self-center flex w-full justify-between my-1">
+					<p>💪: {player?.attackModifier}</p>
+					<button
+						className="bg-blue-500 text-white rounded w-6 ml-3"
+						onClick={lolwhat}
+					>
+						+
+					</button>
 				</div>
-				<div className="shadow self-center text-center">
-					<h4>🛡️</h4>
-					<p>{player?.blockAmount}</p>
+				<div className="shadow self-center flex w-full justify-between">
+					<p>🛡️: {player?.blockAmount}</p>
+					<button
+						className="bg-blue-500 text-white rounded w-6 ml-3"
+						onClick={lolwhat}
+					>
+						+
+					</button>
 				</div>
 			</div>
 		</>
