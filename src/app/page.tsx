a6e056b8fa6e09a27a04d5599f5dd7ecf36f0781
Hunam <hunam@disroot.org>
2023-11-11 18:02:42 +0200

    add player selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,47 @@
+'use client';
+
 import Cheat from './ui/cheat';
 import HealthBar from './ui/healthbar';
 import Skills from './ui/skills';
 import Actions from './ui/actions';
+import { useState } from 'react';
 
-export default async function Home() {
+export default function Home() {
+  const [id, setId] = useState(-1);
   return (
     <main>
-      <Cheat id={1} />
-      <HealthBar id={1} />
-      <Skills id={1} />
-      <Actions id={1} />
+      {id === -1 && (
+        <>
+          <label>
+            <input
+              type='radio'
+              name='radioSet'
+              value='1'
+              checked={id === 1}
+              onChange={() => setId(1)}
+            />
+            Player 1
+          </label>
+          <label>
+            <input
+              type='radio'
+              name='radioSet'
+              value='2'
+              checked={id === 2}
+              onChange={() => setId(2)}
+            />
+            Player 2
+          </label>
+        </>
+      )}
+      {id !== -1 && (
+        <>
+          <Cheat id={id} />
+          <HealthBar id={id} />
+          <Skills id={id} />
+          <Actions id={id} />
+        </>
+      )}
     </main>
   );
 }
