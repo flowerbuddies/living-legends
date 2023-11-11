b535db67ba74f857b2fbca510681571c7bc50044
tspoof <Stte@users.noreply.github.com>
2023-11-11 20:02:01 +0200

    add character and gears (#3)

---
+++ src/app/ui/character.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import Image from "next/image";
+import Gear from "./gear";
+import Skills from "./skills";
+
+export default function Character(props: { id: number }) {
+	return (
+		<div className="flex">
+			<Skills id={props.id} />
+			<div className="self-center mx-3">
+				<Image
+					src="/assets/characters/hero_idle_3.png"
+					alt="character"
+					width={300}
+					height={500}
+				/>
+			</div>
+			<Gear id={props.id} />
+		</div>
+	);
+}
--- src/app/ui/skills.tsx
+++ src/app/ui/skills.tsx
@@ -1,33 +1,46 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { PlayerInfo } from '../../lib/player';
+import { useEffect, useState } from "react";
+import { PlayerInfo } from "../../lib/player";
 
 export default function Skills(props: { id: number }) {
-  const [player, setPlayer] = useState<PlayerInfo>();
+	const [player, setPlayer] = useState<PlayerInfo>();
 
-  useEffect(() => {
-    fetch(`/api/player?id=${props.id}`)
-      .then((res) => res.json())
-      .then((player: any) => {
-        setPlayer(player);
-      });
-  }, []);
-  return (
-    <>
-      <div className='flex flex-col'>
-        <div className='self-center'>
-          <h3>Skills</h3>
-        </div>
-        <div className='shadow self-center text-center'>
-          <h4>💪</h4>
-          <p>{player?.attackModifier}</p>
-        </div>
-        <div className='shadow self-center text-center'>
-          <h4>🛡️</h4>
-          <p>{player?.blockAmount}</p>
-        </div>
-      </div>
-    </>
-  );
+	let lolwhat = () => {};
+
+	useEffect(() => {
+		fetch(`/api/player?id=${props.id}`)
+			.then((res) => res.json())
+			.then((player: any) => {
+				setPlayer(player);
+			});
+	}, []);
+
+	return (
+		<>
+			<div className="flex flex-col">
+				<div className="self-center">
+					<h3>Skills</h3>
+				</div>
+				<div className="shadow self-center flex w-full justify-between my-1">
+					<p>💪: {player?.attackModifier}</p>
+					<button
+						className="bg-blue-500 text-white rounded w-6 ml-3"
+						onClick={lolwhat}
+					>
+						+
+					</button>
+				</div>
+				<div className="shadow self-center flex w-full justify-between">
+					<p>🛡️: {player?.blockAmount}</p>
+					<button
+						className="bg-blue-500 text-white rounded w-6 ml-3"
+						onClick={lolwhat}
+					>
+						+
+					</button>
+				</div>
+			</div>
+		</>
+	);
 }
